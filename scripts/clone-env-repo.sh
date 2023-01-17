59ef07e84f23fe32f12659d8397ce07e4cce08f7
Jean Carlo <jeantardelli@gmail.com>
2023-01-17 23:13:51 +0000

    Trigger CICD part I

--- scripts/clone-env-repo.sh
+++ scripts/clone-env-repo.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-ssh-keyscan -t rsa github.com >> known_hosts.github
-git clone git@github.com:jeantardelli/gitops-style-cicd-with-gcp-env-repo.git && \
+git clone https://github.com/jeantardelli/gitops-style-cicd-with-gcp-env-repo.git && \
 cd gitops-stye-cicd-with-gcp-env-repo && \
 git checkout candidate && \
 git config user.email $(gcloud auth list --filter=status:ACTIVE --format='value(account)')
