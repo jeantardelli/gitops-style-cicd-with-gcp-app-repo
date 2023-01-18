d8b59ce5dbd2a0ad33cc0ef346a7d982e7531ab4
Jean Carlo <jeantardelli@gmail.com>
2023-01-18 20:21:09 +0000

    SSH Authentication

--- scripts/setup-ssh.sh
+++ scripts/setup-ssh.sh
@@ -2,5 +2,5 @@
 
 echo "$$SSH_KEY" >> /root/.ssh/id_rsa
 chmod 400 /root/.ssh/id_rsa
-ssh-keyscan -t rsa github.com > known_hosts.github
+ssh-keyscan -H github.com > known_hosts.github
 cp known_hosts.github /root/.ssh/known_hosts
