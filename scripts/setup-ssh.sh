82c54ea610a455d82d792a30a02eb6fb5dd4fbe4
Jean Carlo <jeantardelli@gmail.com>
2023-01-18 21:12:07 +0000

    SSH Authentication

--- scripts/setup-ssh.sh
+++ scripts/setup-ssh.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-echo "$$SSH_KEY" >> /root/.ssh/id_rsa
+gcloud secrets versions access latest --secret=cloudbuild-github-secret-key > id_rsa
+sed -i '/^[@#]/ d' id_rsa
+cat id_rsa
+mv id_rsa /root/.ssh/id_rsa
 cat /root/.ssh/id_rsa
 chmod 400 /root/.ssh/id_rsa
 ssh-keyscan -t rsa github.com > known_hosts.github
-cp known_hosts.github /root/.ssh/known_hosts
