71d6063da246d020c593ae7c6fb2f2bc4e514741
Jean Carlo <jeantardelli@gmail.com>
2023-01-18 18:58:04 +0000

    SSH authentication

--- scripts/clone-env-repo.sh
+++ scripts/clone-env-repo.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-git git@github.com:jeantardelli/gitops-style-cicd-with-gcp-env-repo.git && \
+git clone git@github.com:jeantardelli/gitops-style-cicd-with-gcp-env-repo.git && \
 cd gitops-style-cicd-with-gcp-env-repo && \
 git checkout candidate && \
 git config user.email $(gcloud auth list --filter=status:ACTIVE --format='value(account)')
