8261506a19a11c86a1790fbbd4fb2dd7b55d8110
Jean Carlo <jeantardelli@gmail.com>
2023-01-19 19:28:21 +0000

    Trigger CI

--- scripts/clone-env-repo.sh
+++ scripts/clone-env-repo.sh
@@ -3,4 +3,4 @@
 git clone git@github.com:jeantardelli/gitops-style-cicd-with-gcp-env-repo.git && \
 cd gitops-style-cicd-with-gcp-env-repo && \
 git checkout candidate && \
-git config user.email $(gcloud auth list --filter=status:ACTIVE --format='value(account)')
+git config --global user.email $(gcloud auth list --filter=status:ACTIVE --format='value(account)')
